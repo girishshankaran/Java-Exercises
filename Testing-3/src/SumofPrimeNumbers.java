946736b5af2883689a1dd79a39711e0d1c5e1f67
test-user <test-user.gmail.com>
2022-06-12 13:29:28 +0530

    changes for Prime numbers

---
+++ Testing-3/src/SumofPrimeNumbers.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class SumofPrimeNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Sum of how many prime numbers?");
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt(); 
+		int i=0; int j; 
+		
+		while (i<n)
+		{
+			int st_num=3;
+		  
+			for (j=3;j<=st_num/i;j++)
+		      {
+				if((j%st_num)==0)
+			     
+		      }
+		}
+		
+
+	}
+
+}
