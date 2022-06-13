2d6b66f826cb45c77c1ca478013074ce9b0ee13e
test-user <test-user.gmail.com>
2022-06-13 22:39:15 +0530

    PrimeandNonprime count

--- Testing-3/src/SumofPrimeNumbers.java
+++ Testing-3/src/SumofPrimeNumbers.java
@@ -1,3 +1,5 @@
+/* This program is for grouping the prime and non-prime members in a given array */
+
 import java.util.Scanner;
 
 public class SumofPrimeNumbers {
@@ -5,23 +7,38 @@ public class SumofPrimeNumbers {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Sum of how many prime numbers?");
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt(); 
-		int i=0; int j; 
+		/* This is a multi-liner comment */
+        // This is a single-liner comment
 		
-		while (i<n)
+		int a[]= {3,5,7,8,9,10,13,15,16,19,17,20};
+		int i,j; 
+		int check=1;
+		int countPrime=0;
+		
+		for (j=0;j<a.length;j++)
 		{
-			int st_num=3;
-		  
-			for (j=3;j<=st_num/i;j++)
-		      {
-				if((j%st_num)==0)
-			     
-		      }
-		}
 		
-
+			for (i=4;i<=a[j]/2;i++)
+				{  
+				if((a[j]%2)==0)
+					{
+					System.out.println(a[j] + " is NOT a prime number");
+					check=0;
+					break;
+				    /* If check=0, then break the inner loop and move to the next number for checking */
+					}
+            			
+				}
+      
+			if (check==1)
+			{
+				System.out.println(a[j] + " is a prime number");
+				countPrime++;
+			}
+		
+		 }	
+		System.out.println("Total prime numbers = " + countPrime);
+		System.out.println("Total non-prime numbers = " + (a.length-countPrime));
 	}
 
 }
